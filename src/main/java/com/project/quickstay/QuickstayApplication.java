77f8680434642dc7cba64dba1d64b1f14aa09903
thstkddnr20 <thstkddnr20@naver.com>
2025-02-02 17:15:20 +0900

    initial commit

---
+++ src/main/java/com/project/quickstay/QuickstayApplication.java
@@ -0,0 +1,13 @@
+package com.project.quickstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/quickstay/QuickstayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quickstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
