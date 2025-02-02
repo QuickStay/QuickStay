2393afe2c2c3737172ed86d33fe586c1202c4ed6
KKPASII <kkongpasi1005@gmail.com>
2025-02-02 18:08:22 +0900

    Room ENTITY 생성

---
+++ src/main/java/com/project/quickstay/domain/Room.java
@@ -0,0 +1,24 @@
+package com.project.quickstay.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Place place;
+
+    private String name;
+
+    private String description;
+
+    private Integer capacity;
+
+    public Room() {
+    }
+}
