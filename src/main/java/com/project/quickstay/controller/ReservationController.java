1aa5aec2edcf4820c6ea5c9523491c385160bd6f
KKPASII <kkongpasi1005@gmail.com>
2025-02-16 17:57:13 +0900

    내 예약 버튼, 내 예약 목록 생성

--- src/main/java/com/project/quickstay/controller/ReservationController.java
+++ src/main/java/com/project/quickstay/controller/ReservationController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -33,5 +34,9 @@ public class ReservationController {
         return "redirect:/home";
     }
 
+    @GetMapping("/reservation/list")
+    public String reservationList() {
 
+        return "myPage/myReservation";
+    }
 }
