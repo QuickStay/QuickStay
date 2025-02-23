af276124de40fc595c40585fc68a6eb72c393284
thstkddnr20 <thstkddnr20@naver.com>
2025-02-23 15:54:28 +0900

    log 확인용

--- src/main/java/com/project/quickstay/controller/PlaceApiController.java
+++ src/main/java/com/project/quickstay/controller/PlaceApiController.java
@@ -3,6 +3,7 @@ package com.project.quickstay.controller;
 import com.project.quickstay.domain.place.dto.PlaceSearch;
 import com.project.quickstay.service.PlaceService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class PlaceApiController {
 
     private final PlaceService placeService;
@@ -18,6 +20,7 @@ public class PlaceApiController {
     @GetMapping("/api/place/search")
     public ResponseEntity<Page<PlaceSearch>> placeSearch(@RequestParam(defaultValue = "1", required = false) int page, @RequestParam String keyword) {
         Page<PlaceSearch> search = placeService.search(keyword, page);
+        log.info("로그");
         return ResponseEntity.ok(search);
     }
 }
