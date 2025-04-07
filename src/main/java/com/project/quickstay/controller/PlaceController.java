1531faef1b6fc60a912bd083a76c1f1c89caf2ed
thstkddnr20 <thstkddnr20@naver.com>
2025-04-07 19:59:42 +0900

    fix: 지도 임시 제거

--- src/main/java/com/project/quickstay/controller/PlaceController.java
+++ src/main/java/com/project/quickstay/controller/PlaceController.java
@@ -81,7 +81,6 @@ public class PlaceController {
         List<ReviewInfo> reviews = reviewService.getPlaceReview(placeId);
         model.addAttribute("info", info);
         model.addAttribute("reviews", reviews);
-        model.addAttribute("jsKey", kakaoJsKey);
         return "place/placeInfo";
     }
 
