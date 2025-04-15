7c1fb4efe24d7b9f0157301ede32d40d1cc329bc
thstkddnr20 <thstkddnr20@naver.com>
2025-04-15 23:38:00 +0900

    refactor: 불필요한 의존성, 주석, 로그 출력 제거

--- src/main/java/com/project/quickstay/controller/LoginController.java
+++ src/main/java/com/project/quickstay/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.project.quickstay.controller;
 
-import com.project.quickstay.common.KakaoProvider;
 import com.project.quickstay.domain.user.entity.User;
 import com.project.quickstay.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,7 +18,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
-    private final KakaoProvider kakaoProvider;
+
     private final LoginService loginService;
 
     @Value("${spring.kakao.client_id}")
@@ -29,11 +28,9 @@ public class LoginController {
     String redirectUri;
 
     @GetMapping("/oauth/kakao")
-    public void kakaoLogin(HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public void kakaoLogin(HttpServletResponse response) throws IOException {
         String url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId
                 + "&redirect_uri=" + redirectUri;
-
-        log.info("url = {}", url);
 
         response.sendRedirect(url);
     }
@@ -55,19 +52,16 @@ public class LoginController {
             session.setMaxInactiveInterval(60 * 60); // 3600초 : 1시간
         }
 
-
-
-        // redirect 주소
         String redirectURL = "/home";
 
-        return "redirect:"+redirectURL;
+        return "redirect:" + redirectURL;
     }
 
     @GetMapping("/logout")
     public String logout(final HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) {
-            session.invalidate(); // session 속 모든 속성값 삭제
+            session.invalidate();
         }
 
         return "redirect:/home";
