6638f2be046fd366dff357477c721a2575dc6810
손상욱 <thstkddnr20@naver.com>
2025-04-16 13:42:17 +0900

    fix: 로그인 안했을 경우 false 반환하여 인터셉터 실행 중지

--- src/main/java/com/project/quickstay/common/interceptor/NewUserInterceptor.java
+++ src/main/java/com/project/quickstay/common/interceptor/NewUserInterceptor.java
@@ -5,14 +5,17 @@ import com.project.quickstay.domain.user.entity.UserType;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Slf4j
 public class NewUserInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession(false);
         if (session == null) {
             response.sendRedirect("/login");
+            return false;
         }
         User user = (User) session.getAttribute("loginUser");
 
